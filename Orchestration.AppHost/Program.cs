014c3c396fcbf6845e859da8fabef630da3b22c6
nhan.tran@bowaresolutions.com <Nhan Boware>
2025-07-09 14:36:29 +0700

    Refactor: gateway and identity for development

--- Orchestration.AppHost/Program.cs
+++ Orchestration.AppHost/Program.cs
@@ -2,7 +2,9 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var redis = builder.AddRedis("redis");
 
-var productService = builder.AddProject<Projects.ProductService_Api>("product-service").WithReference(redis);
+var gateway = builder.AddProject<Projects.APIGateway>("gateway");
+var authService = builder.AddProject<Projects.AuthService>("auth-service");
+var productService = builder.AddProject<Projects.ProductService_Api>("product-service");
 var userService = builder.AddProject<Projects.UserService_Api>("user-service").WithReference(redis);
 
 builder.Build().Run();

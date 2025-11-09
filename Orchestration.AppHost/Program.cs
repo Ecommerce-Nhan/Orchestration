abf45183860a6e275b07569a0dbacc138d02fac7
Trần Thanh Nhân <nhancmvn12@gmail.com>
2025-11-09 14:08:51 +0700

    Refactor: restructure orchestration

--- Orchestration.AppHost/Program.cs
+++ Orchestration.AppHost/Program.cs
@@ -1,10 +1,6 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var redis = builder.AddRedis("redis");
-
-var gateway = builder.AddProject<Projects.APIGateway>("gateway");
-var authService = builder.AddProject<Projects.AuthService>("auth-service");
 var productService = builder.AddProject<Projects.ProductService_Api>("product-service");
-var userService = builder.AddProject<Projects.UserService_Api>("user-service").WithReference(redis);
+var userService = builder.AddProject<Projects.UserService_Api>("user-service");
 
 builder.Build().Run();
